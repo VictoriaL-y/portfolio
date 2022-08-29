e6962815874b214c75f18ae0634071951b624d67
Victoria Lipnitsky <victoria.lipnitsky@gmail.com>
2022-08-29 22:28:15 +0200

    Changed the welcome section, a link ant the footer

--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,20 @@ let timeout = setTimeout(() => {
 }, 5000);
 
 
+// Here we are getting a full-size responsive image in the welcome section on a mobile device. 
+//First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
+
 // Here we adjust a current year
 const year = document.querySelector('#current-year')
 year.innerHTML = new Date().getFullYear()
